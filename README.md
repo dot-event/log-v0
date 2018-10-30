8ad98c035afdfcb60e579411728e0bbf26729430
winton <mail@wintoni.us>
2018-10-30 14:45:16 -0400

    Event logging test, name change

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# @dot-event/log
+# @dot-store/log
 
-Logger for `dot-event`.
+Logger for `dot-store`.
 
 ![logs](https://66.media.tumblr.com/117793b9fb72596a9a72f7611b0eb908/tumblr_nylfvsewAa1qgwu5ho1_500.gif)
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,34 @@
 import dotEvent from "dot-event"
 import dotStore from "dot-store"
+import { readFile } from "fs-extra"
 import log from "../dist/log"
 
-test("log", async () => {
+test("logs text", async () => {
   const events = dotEvent()
   const store = dotStore(events)
 
-  log({ events, store })
+  log({ events, keep: false, store })
 
-  await events.log("writeJson.test", ["hello"])
+  await events.log("test", ["hello"])
+  const out = await readFile(store.get("log.test.path"))
+
+  expect(out.toString()).toMatch(/hello/)
+})
+
+test("logs events", async () => {
+  const events = dotEvent()
+  const store = dotStore(events)
+
+  log({ events, keep: false, store })
+
+  events.onAny(async ({ event }) => {
+    if (event.op !== "log") {
+      await events.log("any", { event })
+    }
+  })
+
+  await events.emit("hello", "world")
+  const out = await readFile(store.get("log.any.path"))
+
+  expect(out.toString()).toMatch(/emit\thello\t"world"/)
 })

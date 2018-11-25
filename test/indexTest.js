8d1e1a14fecf4c859371c9ecafcd1e7705f2afe3
winton <mail@wintoni.us>
2018-11-25 16:58:42 -0500

    Upgrade @dot-event/store

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,29 +1,32 @@
+// Packages
 import dotEvent from "dot-event"
 import dotStore from "@dot-event/store"
 import { readFile } from "fs-extra"
 
-import dotLog from "../dist/log"
+// Helpers
+import dotLog from "../"
 
+// Tests
 test("logs text", async () => {
   const events = dotEvent()
-  const store = dotStore({ events })
 
-  dotLog({ events, store })
+  dotLog({ events })
+  dotStore({ events })
 
   await events.log("test", ["hello"])
-  const out = await readFile(store.get("test.log"))
+  const out = await readFile(events.get("test.log"))
 
   expect(out.toString()).toMatch(/hello/)
 })
 
 test("logs events", async () => {
   const events = dotEvent()
-  const store = dotStore({ events })
 
-  dotLog({ events, store })
+  dotLog({ events })
+  dotStore({ events })
 
   await events.emit("hello", "world")
-  const out = await readFile(store.get("any.log"))
+  const out = await readFile(events.get("any.log"))
 
   expect(out.toString()).toMatch(/emit\thello\t"world"/)
 })
